00adb344ad52bbe9d135aa557082711437141a65
Abhinav KM <abhinavkm184@gmail.com>
2018-03-10 15:57:00 +0530

    byjus auto fetcher js

--- byju.js
+++ byju.js
@@ -5,7 +5,8 @@ function beep(){
     try{
         var len = m.firstChild.children[1].childNodes[0].firstChild.childNodes[0].childNodes[0].childNodes[0].childNodes[2].childNodes[0].childNodes[0].childNodes[2].childNodes[2].childNodes[1].length;
         if ( len != 0){
-            x = null;
+            // question fetched
+            x = 0;
             var audio = new Audio("https://www.soundjay.com/button/beep-08b.mp3");
             audio.play();
         }
@@ -20,10 +21,13 @@ function beep(){
 
 function tick() { 
     beep();
-    if ( x != null ){
+    if ( x != null && x != 0 ){
         x[1].click();    
     }
-    else{
+    else if( x == null ){
+        x = document.getElementsByClassName("undefined");
+    }
+    else if( x == 0) {
         console.log("Question fetched...");
         beep();
     }
